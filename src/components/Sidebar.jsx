c9c760473c25436890dec9bab5c19a7f5fe15bba
Hammad Khaliq <hammadkhaliq987@gmail.com>
2025-09-09 07:49:38 +0500

    Initial commit of the project

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Sidebar(){
+
+    return(
+        <div>
+            <h1 className="h-14 width-1/2 bg-linear-65 from-green-500 to-orange-500">Ai Assistant</h1>
+        </div>
+    )
+}
+
+export default Sidebar;
